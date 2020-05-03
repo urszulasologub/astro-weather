8b4c4bf33a8dfdc18d1720997ff57f1eebc121d7
Urszula Sołogub <gold.ula4@gmail.com>
2020-05-04 00:03:36 +0200

    Added thread updating time on FragmentView

--- app/src/main/java/com/example/astroweather/FragmentView.java
+++ app/src/main/java/com/example/astroweather/FragmentView.java
@@ -1,14 +1,90 @@
 package com.example.astroweather;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.astrocalculator.AstroDateTime;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class FragmentView extends AppCompatActivity {
+
+	private int day;
+	private int month;
+	private int year;
+	private int hour;
+	private int minute;
+	private int second;
+	private Double x;
+	private Double y;
+	private Thread update_time_thread;
+
+
+	private void updateDateTime() {
+		Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+		Date date = calendar.getTime();
+		day = calendar.get(Calendar.DATE);
+		month = calendar.get(Calendar.MONTH) + 1;
+		year = calendar.get(Calendar.YEAR);
+		hour = calendar.get(Calendar.HOUR_OF_DAY);
+		minute = calendar.get(Calendar.MINUTE);
+		second = calendar.get(Calendar.SECOND);
+	}
+
+	private void setTime(TextView current_time) {
+		@SuppressLint("DefaultLocale") String time = String.format("%02d:%02d:%02d", hour, minute, second);
+		current_time.setText(time);
+	}
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_view);
+
+		Intent this_intent = getIntent();
+		x = this_intent.getDoubleExtra("x", 0);
+		y = this_intent.getDoubleExtra("y", 0);
+
+		TextView x_label = (TextView)findViewById(R.id.x_label);
+		x_label.setText(Double.toString(x));
+		TextView y_label = (TextView)findViewById(R.id.y_label);
+		y_label.setText(Double.toString(y));
+
+		final TextView current_time = (TextView)findViewById(R.id.current_time);
+		updateDateTime();
+		setTime(current_time);
+
+		update_time_thread = new Thread() {
+
+			@Override
+			public void run() {
+				try {
+					while (!update_time_thread.isInterrupted()) {
+						Thread.sleep(1000);
+						runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								updateDateTime();
+								setTime(current_time);
+							}
+						});
+					}
+				} catch (InterruptedException e) {
+				}
+			}
+		};
+
+		update_time_thread.start();
+
+
 	}
 }
